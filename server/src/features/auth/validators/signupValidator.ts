cf45537d4474281f3a929cf7cef55eeb281a2957
alyQamar <alyeqee@gmail.com>
2023-10-27 20:24:29 +0200

    Fixed validator error and edit model parameters

--- server/src/features/auth/validators/signupValidator.ts
+++ server/src/features/auth/validators/signupValidator.ts
@@ -1,6 +1,6 @@
 import Joi, { ObjectSchema } from 'joi';
 
-const signupValidator: ObjectSchema = Joi.object({
+const signupValidator: ObjectSchema = Joi.object().keys({
   name: Joi.string().min(3).max(15).required().messages({
     'string.base': 'Username must be of type string',
     'string.min': 'Username should have at least {#limit} characters',
@@ -8,17 +8,12 @@ const signupValidator: ObjectSchema = Joi.object({
     'any.required': 'Username is a required field',
     'string.empty': 'Username is not allowed to be empty'
   }),
-  password: Joi.string().min(3).max(9).required().messages({
+  password: Joi.string().min(3).max(100).required().messages({
     'string.base': 'Password must be of type string',
     'string.min': 'Password should have at least {#limit} characters',
     'string.max': 'Password should have at most {#limit} characters',
     'any.required': 'Password is a required field',
     'string.empty': 'Password is not allowed to be empty'
-  }),
-  confirmPassword: Joi.string().valid(Joi.ref('password')).required().messages({
-    'any.only': 'Passwords do not match',
-    'any.required': 'Confirm Password is a required field',
-    'string.empty': 'Confirm Password is not allowed to be empty'
   }),
   email: Joi.string().email().required().messages({
     'string.base': 'Email must be of type string',
