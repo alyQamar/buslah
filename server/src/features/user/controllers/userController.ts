c925636d3175de281b7675d3d226f22e6a2241d6
OmarAbdulwahab <omarabdulwahab982@gmail.com>
2023-11-02 16:46:20 +0200

    add the logic to getUser by ID

--- server/src/features/user/controllers/userController.ts
+++ server/src/features/user/controllers/userController.ts
@@ -1,13 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
 import { config } from '@config/index';
 import { validate } from '@global/middlewares/validationMiddleware';
-import User from '@user/models/userModel';
-import dotenv from 'dotenv';
-dotenv.config({ path: '.env' });
+import userModel from '@user/models/userModel';
+import { IUserDocument } from '@user/interfaces/userInterface';
+import { createCommonService, commonFunctions } from '@service/db/common.services';
+import { NotFoundError } from '@global/middlewares/errorMiddleware';
+
+const CRUDFunctions: commonFunctions<IUserDocument> = createCommonService<IUserDocument>(userModel);
 
 class userController {
-  public async getUserByID(req: Request, res: Response, next: NextFunction): Promise<void> {
-    // TODO create getOne function on the factory then use it here
+  public async getUser(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      await CRUDFunctions.getOne(req, res);
+    } catch (error) {
+      throw new NotFoundError('Cannot find the user.');
+    }
   }
 }
 
