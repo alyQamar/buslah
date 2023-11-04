eaf0fc4bf01ff76ba93091bd94da666578191c92
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-11-04 17:59:35 +0200

    create function to generate JWT token

--- server/src/features/auth/controllers/authController.ts
+++ server/src/features/auth/controllers/authController.ts
@@ -1,4 +1,4 @@
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
@@ -9,10 +9,10 @@ import Auth, { IAuthDocument } from '@auth/models/Auth';
 import { string } from 'joi';
 
 class authController {
-  // public createToken(userId: string): string {
-  //   const token = jwt.sign({ id: userId }, 'R3Fkw7mD2Lp6eP@!5EA-368&7843?5Eq', { expiresIn: process.env.JWT_EXPIRE_TIME });
-  //   return token;
-  // }
+  public static createToken = (userId: ObjectId): string => {
+    const token = jwt.sign({ id: userId }, config.JWT_SECRET_KEY, { expiresIn: process.env.JWT_EXPIRE_TIME });
+    return token;
+  };
 
   @validate(signupValidator)
   public async SignUp(req: Request, res: Response, next: NextFunction): Promise<void> {
@@ -36,8 +36,9 @@ class authController {
       // Creating a new document using the Auth model
       const newDoc = await Auth.create(data);
 
-      // Call the `createToken()` method to generate a JWT token
-      const jwtToken = jwt.sign({ id: newDoc._id }, config.JWT_SECRET_KEY, { expiresIn: config.JWT_EXPIRE_TIME });
+      // generate a JWT token
+      const id = newDoc._id as unknown as ObjectId;
+      const jwtToken = authController.createToken(id);
 
       // Sending a response with the newly created document and the JWT token
       res.status(201).json({ status: 'success', token: jwtToken, data: newDoc });
