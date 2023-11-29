c46ce622e8fa9288da9ca82c44dd2c293e82f749
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-11-29 15:36:24 +0200

    resetPasswordvalidator functionality

--- server/src/features/auth/auth.validators.ts
+++ server/src/features/auth/auth.validators.ts
@@ -23,4 +23,20 @@ const signupValidator: ObjectSchema = Joi.object().keys({
   })
 });
 
-export { signupValidator };
+const resetPasswordValidator: ObjectSchema = Joi.object().keys({
+  email: Joi.string().email().required().messages({
+    'string.base': 'Email must be of type string',
+    'string.email': 'Email must be valid',
+    'any.required': 'Email is a required field',
+    'string.empty': 'Email is not allowed to be empty'
+  }),
+  password: Joi.string().min(8).max(100).required().messages({
+    'string.base': 'Password must be of type string',
+    'string.min': 'Password should have at least {#limit} characters',
+    'string.max': 'Password should have at most {#limit} characters',
+    'any.required': 'Password is a required field',
+    'string.empty': 'Password is not allowed to be empty'
+  })
+});
+
+export { signupValidator, resetPasswordValidator };
