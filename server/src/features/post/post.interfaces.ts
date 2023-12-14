76fa1c1329d2376245bb0f9b83ca006c6f6c0a4f
alyQamar <alyeqee@gmail.com>
2023-12-14 16:53:51 +0200

    Handle post integration with comment and reaction

--- server/src/features/post/post.interfaces.ts
+++ server/src/features/post/post.interfaces.ts
@@ -1,7 +1,6 @@
-import { ObjectId } from 'mongodb';
-import { Document } from 'mongoose';
+import mongoose, { Document } from 'mongoose';
 import { ICommentDocument } from '@comment/comment.interfaces';
-// import { IReactionsDocument } from '@reactions/reactions.interfaces';
+import { IReactionDocument } from '@reaction/reaction.interfaces';
 
 export enum PrivacyOptions {
   Public = 'public',
@@ -17,22 +16,22 @@ export enum Feelings {
 
 
 interface IPostBase {
-  content: string;
+  post: string;
   bgColor: string;
-  feelings?: Feelings;
   // imgVersion?: string;
   // imgId?: string;
   // videoId?: string;
   // videoVersion?: string;
+  feelings?: Feelings;
   privacy?: PrivacyOptions;
   shares: number;
-  // reactions?: IReactionsDocument[];
+  reactions?: IReactionDocument[];
   comments?: ICommentDocument[];
 }
 
 export interface IPostDocument extends Document, IPostBase {
-  _id: ObjectId;
-  parentPost?: ObjectId;
-  prevPost?: ObjectId;
-  user: ObjectId;
+  _id: mongoose.Types.ObjectId | string;
+  parentPost?: mongoose.Types.ObjectId | string;
+  prevPost?: mongoose.Types.ObjectId | string;
+  user: mongoose.Types.ObjectId | string;
 }
--- server/src/features/post/post.model.ts
+++ server/src/features/post/post.model.ts
@@ -1,34 +1,70 @@
-import { ObjectId } from 'mongodb';
-import { Schema, model, Model } from 'mongoose';
-
+import mongoose, { model, Model, Schema } from 'mongoose';
 import { IPostDocument, Feelings, PrivacyOptions } from '@post/post.interfaces';
+
 
 const postSchema: Schema = new Schema({
   // -----------------------------------------------------
   // referenced to another documents
-  parentPost: { type: ObjectId, ref: 'Post', index: true },
-  prevPost: { type: ObjectId, ref: 'Post', index: true },
-  user: { type: ObjectId, ref: 'User', index: true },
-  bookmarksBy: [{ type: ObjectId, ref: 'User' }],
-  // -----------------------------------------------------
-  content: { type: String, default: '' },
-  bgColor: { type: String, default: '' },
-  feelings: { type: String, enum: Object.values(Feelings), default: '' },
-  privacy: { type: String, enum: Object.values(PrivacyOptions), default: PrivacyOptions.Public },
-  reactions: [{ type: ObjectId, ref: 'Reaction' }],
-  comments: [{ type: ObjectId, ref: 'Comment' }],
-  commentsCount: { type: Number, default: 0 },
-  reactionsCount: {
-    like: { type: Number, default: 0 },
-    love: { type: Number, default: 0 },
-    celebrate: { type: Number, default: 0 },
-    angry: { type: Number, default: 0 },
+  parentPost: {
+    type: String || mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+    index: true
   },
+  prevPost: {
+    type: String || mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+    index: true
+  },
+  user: {
+    type: String || mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  },
+  bookmarksBy: [{
+    type: String || mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  // -----------------------------------------------------
+  post: {
+    type: String,
+    default: '',
+    required: true
+  },
+  bgColor: {
+    type: String,
+    default: ''
+  },
+  feelings: {
+    type: String,
+    enum: Object.values(Feelings)
+  },
+  privacy: {
+    type: String,
+    enum: Object.values(PrivacyOptions),
+    default: PrivacyOptions.Public
+  },
+  reactions: [{
+    type: String || mongoose.Schema.Types.ObjectId,
+    ref: 'Reaction'
+  }],
+  comments: [{
+    type: String || mongoose.Schema.Types.ObjectId,
+    ref: 'Comment'
+  }],
   sharingNumbers: {
-    shares: { type: Number, default: 0 },
-    bookmarks: { type: Number, default: 0 },
+    shares: {
+      type: Number,
+      default: 0
+    },
+    bookmarks: {
+      type: Number,
+      default: 0
+    },
   }
 }, { timestamps: true });
+
+
 // Mongoose query middleware
 postSchema.pre(/^find/, function (next) {
   this.populate('user'); // Populate the 'user' field
@@ -37,14 +73,12 @@ postSchema.pre(/^find/, function (next) {
 
   this.populate({
     path: 'reactions',
-    select: 'name -_id',
+    select: 'reaction userID _id',
   });
+
   this.populate({
     path: 'comments',
-    populate: {
-      path: 'reactions',
-      select: 'name -_id',
-    },
+    select: 'comment user _id',
   });
   next();
 });
