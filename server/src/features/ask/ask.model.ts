d000f9ebe2032fe81ba5479643508b68721e25d5
OmarAbdulwahab <omarabdulwahab982@gmail.com>
2024-02-06 19:28:51 +0200

    Created ask model

---
+++ server/src/features/ask/ask.model.ts
@@ -0,0 +1,16 @@
+import mongoose, { model, Model, Schema } from 'mongoose';
+import { IAskDocument } from '@ask/ask.interface';
+
+const askSchema: Schema = new Schema({
+  userID: {
+    type: String || mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: [true, 'Reaction must belong to a user.']
+  },
+  question: {
+    type: String,
+    required: [true, 'An ask must have a question']
+  }
+});
+
+export const askModel: Model<IAskDocument> = model<IAskDocument>('Ask', askSchema);
