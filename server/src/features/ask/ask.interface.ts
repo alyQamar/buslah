a56c74b67132480b4591aa1f3c576ddfef7ee935
alyQamar <alyeqee@gmail.com>
2024-02-08 05:34:07 +0200

    Enhance ask and Integration between ask and answer

--- server/src/features/ask/ask.interface.ts
+++ server/src/features/ask/ask.interface.ts
@@ -1,7 +1,28 @@
 import mongoose, { Document } from 'mongoose';
 
-export interface IAskDocument extends Document {
-  userID: mongoose.Types.ObjectId;
+import { PrivacyOptions } from '@auth/auth.interfaces';
+import { IAnswerDocument } from '@answer/answer.interfaces';
+
+
+export enum Feelings {
+  Bug = 'bug',
+  Confused = 'confused',
+  Stuck = 'stuck',
+  Anxious = 'anxious',
+  Motivated = 'motivated',
+}
+
+export interface IAskBase extends Document {
   question: string;
+  helpfulUsers: mongoose.Types.ObjectId[] | string[];
+  unhelpfulUsers: mongoose.Types.ObjectId[] | string[];
+  bookmarksBy: mongoose.Types.ObjectId[] | string[];
+  answers: IAnswerDocument[];
   locked?: boolean;
+  feelings?: Feelings;
+  privacy?: PrivacyOptions;
+}
+
+export interface IAskDocument extends IAskBase {
+  user: mongoose.Types.ObjectId;
 }
