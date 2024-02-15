ffe912ebaaf506490a08d511850db025f37cb87f
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2024-02-15 14:49:53 +0200

    create upload function and handle profile case

--- server/src/features/media/media.controllers.ts
+++ server/src/features/media/media.controllers.ts
@@ -42,4 +42,53 @@ class MediaController {
       limits: { fileSize: 1024 * 1024 } // 1 megabyte
     });
   }
+
+  public async uploadPhoto(req: Request, res: Response, next: NextFunction) {
+    //  validation
+
+    if (!req.file) {
+      return res.status(400).json({ message: 'no file provided ' });
+    }
+
+    //  Get the path of the image
+
+    const imagePath = path.join(__dirname, `../../../images/${req.file.filename}`);
+
+    //  upload to cloudinary
+
+    const result: { secure_url: string; public_id: string } = (await clodService.uploadImage(imagePath)) as any;
+
+    //  Get the user from DB
+    const { Id, type } = req.params;
+    const user = await UserModel.findById(Id);
+
+    if (type === 'profile') {
+      //  Delete the old profile photo if exist
+      if (user && user.profilePhoto && user.profilePhoto !== null) {
+        console.log('here');
+        const photo = await MediaModel.findById(user.profilePhoto);
+        if (photo) {
+          await clodService.removeImage(photo.publicId);
+          photo.url = result.secure_url;
+          photo.publicId = result.public_id;
+          await photo.save();
+        }
+      } else if (user) {
+        // Create new document for the media
+        const data = {
+          user: Id,
+          mediaType: MediaType.Image,
+          type: ImageType.Profile,
+          url: result.secure_url,
+          publicId: result.public_id,
+          size: req.file.size,
+          mimeType: req.file.mimetype,
+          originalName: req.file.filename
+        };
+        const newMedia = await MediaModel.create(data);
+        user.profilePhoto = newMedia._id;
+        await user.save();
+      }
+    }
+  }
 }
