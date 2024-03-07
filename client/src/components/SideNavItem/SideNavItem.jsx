8b53583e1609eca70bd638eaa6d42d6503d956db
alyQamar <alyeqee@gmail.com>
2024-03-07 14:34:34 +0200

    Convert navItem font weight to medium with tailwind css style

--- client/src/components/SideNavItem/SideNavItem.jsx
+++ client/src/components/SideNavItem/SideNavItem.jsx
@@ -2,7 +2,7 @@ const SideNavItem = ({ name, icon }) => {
   return (
     <div className="w-[236px] h-11 px-3 py-2 justify-start items-center gap-2 inline-flex">
       <img src={icon} alt={name} className="icon w-6 h-6 relative " />
-      <div className="grow shrink basis-0 text-gray-700 text-base font-normal font-['Public Sans'] leading-normal">
+      <div className="grow shrink basis-0 text-gray-700 text-base font-medium font-['Public Sans'] leading-normal ">
         <span>{name}</span>
       </div>
     </div>
