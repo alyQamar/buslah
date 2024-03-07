948c63d1265db03462c2f3aab68a28016e95e1d2
alyQamar <alyeqee@gmail.com>
2024-03-07 17:16:55 +0200

    Handle active sideNavItem

--- client/src/components/SideNav/SideNav.jsx
+++ client/src/components/SideNav/SideNav.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import { sideNavItems } from '@utils/data';
 import SideNavItem from '@components/SideNavItem/SideNavItem';
@@ -6,11 +6,19 @@ import PostButton from '@components/PostButton/PostButton';
 import Footer from '@components/Footer/Footer';
 
 const SideNav = () => {
+  const isSelectedRoute = (name) => {
+    return useLocation().pathname.includes(name.toLowerCase());
+  };
+
   return (
     <div className="fixed side-nav w-[284px] bg-stone-50 rounded-2xl flex flex-col items-center pt-[30px]">
       <div className="flex flex-col gap-[7px] mb-[10px]">
-        {sideNavItems.map((navItem, index) => (
-          <Link to={navItem.route} className="side-nav-item rounded-lg" key={index}>
+        {sideNavItems.map((navItem) => (
+          <Link
+            to={navItem.route}
+            className={`side-nav-item${isSelectedRoute(navItem.name) ? '-active' : ''} rounded-lg`}
+            key={navItem.index}
+          >
             <SideNavItem name={navItem.name} icon={navItem.icon} />
           </Link>
         ))}
