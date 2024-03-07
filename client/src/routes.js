d896cf07e06fef7a6cd565f2ab9e3172bba57476
alyQamar <alyeqee@gmail.com>
2024-03-07 07:15:46 +0200

    Enhance App Routes

---
+++ client/src/routes.js
@@ -0,0 +1,12 @@
+export const routes = {
+  social: '/social',
+  home: '/social/home',
+  mentors: '/social/explore',
+  login: '/login',
+  signup: '/signup',
+  forgetPassword: '/forgetpassword',
+  checkEmail: '/checkemail',
+  resetPassword: '/resetpassword',
+  verifyEmail: '/verifyemail',
+  verified: '/verified'
+};
